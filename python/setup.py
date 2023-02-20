624aa638c3afd2f99aba2115282910702c60882a
Finlay Clark <finlayclark@ed.ac.uk>
2023-02-20 10:03:24 +0000

    Add setup.py for easy installation

---
+++ python/setup.py
@@ -0,0 +1,11 @@
+# setup.py
+import setuptools
+
+setuptools.setup(
+    name="pipeline",
+    version="0.0.1",
+    author="Anna Herz",
+    author_email="Anna.Herz@ed.ac.uk",
+    description="Library for the setup and anlysis of RBFE calculations with AMBER, SOMD, and GROMACS",
+    packages=setuptools.find_packages(),
+)
